4ab38deb7633d8eefec32b16e837ac90b745462a
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2021-05-03 17:03:39 +0700

    change response json

--- assigment2/models/item.go
+++ assigment2/models/item.go
@@ -1,9 +1,9 @@
 package models
 
 type Item struct {
-	ItemID      int    `gorm:"column:item_id;type:int;primary_key;default:nextval('items_id_seq')"`
-	ItemCode    string `gorm:"column:item_code;type:varchar(30)"`
-	Description string `gorm:"column:description;type:text"`
-	Quantity    int    `gorm:"column:quantity;type:int"`
-	OrderID     int    `gorm:"column:order_id;type:int"`
+	ItemID      int    `gorm:"column:item_id;type:int;primary_key;default:nextval('items_id_seq')" json:"lineItemId"`
+	ItemCode    string `gorm:"column:item_code;type:varchar(30)" json:"itemCode"`
+	Description string `gorm:"column:description;type:text" json:"description"`
+	Quantity    int    `gorm:"column:quantity;type:int" json:"quantity"`
+	OrderID     int    `gorm:"column:order_id;type:int" json:"-"`
 }
--- assigment2/models/order.go
+++ assigment2/models/order.go
@@ -3,8 +3,8 @@ package models
 import "time"
 
 type Order struct {
-	OrderID      int       `gorm:"column:order_id;type:int;primary_key;default:nextval('orders_id_seq')"`
-	CustomerName string    `gorm:"column:customer_name;type:varchar(50)"`
-	OrderedAt    time.Time `gorm:"column:ordered_at"`
-	Items        []Item    `gorm:"foreignkey:OrderID"`
+	OrderID      int       `gorm:"column:order_id;type:int;primary_key;default:nextval('orders_id_seq')" json:"orderId"`
+	CustomerName string    `gorm:"column:customer_name;type:varchar(50)" json:"customerName"`
+	OrderedAt    time.Time `gorm:"column:ordered_at" json:"orderedAt"`
+	Items        []Item    `gorm:"foreignkey:OrderID" json:"items"`
 }
