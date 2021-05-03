c9e941a81202fc735193415f9712f33bda2ffa10
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2021-05-03 17:13:46 +0700

    add field data on api response

--- assigment2/models/api.go
+++ assigment2/models/api.go
@@ -6,6 +6,7 @@ type ErrorResponse struct {
 }
 
 type APIResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data,omitempty"`
 }
