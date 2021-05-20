218c3e550deef791abdd02d11d48873522f8ea45
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2021-05-20 16:28:49 +0700

    create function selected data and print in assigment1

--- assigment1/biodata.go
+++ assigment1/biodata.go
@@ -41,21 +41,32 @@ func main() {
 		},
 	}
 	if len(os.Args) > 1 {
-		pilih, err := strconv.Atoi(os.Args[1])
+		input, err := strconv.Atoi(os.Args[1])
 		if err != nil {
 			fmt.Println("Maaf terjadi kesalahan : ", err)
 		}
 		var selectedData Biodata
-		if pilih > 0 && pilih <= len(data) {
-			selectedData = data[pilih-1]
-			fmt.Println("Nama : ", selectedData.Nama)
-			fmt.Println("Alamat : ", selectedData.Alamat)
-			fmt.Println("Pekerjaan : ", selectedData.Pekerjaaan)
-			fmt.Println("Alasan Memilih Kelas Golang : ", selectedData.AlasanMemilihKelasGolang)
+		if input > 0 && input <= len(data) {
+			selectedData = findSelected(data, input)
+			printData(selectedData)
 		} else {
 			fmt.Println("Maaf data yang anda pilih tidak ditemukan.")
 		}
 	} else {
 		fmt.Println("Maaf, Harap masukan pilihan anda")
 	}
+}
+
+func findSelected(data []Biodata, input int) Biodata {
+	if len(data) > 0 {
+		return data[input-1]
+	}
+	return Biodata{}
+}
+
+func printData(data Biodata) {
+	fmt.Println("Nama : ", data.Nama)
+	fmt.Println("Alamat : ", data.Alamat)
+	fmt.Println("Pekerjaan : ", data.Pekerjaaan)
+	fmt.Println("Alasan Memilih Kelas Golang : ", data.AlasanMemilihKelasGolang)
 }
